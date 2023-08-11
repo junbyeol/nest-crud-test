3e3442065621f48f5c8605185cbff2d455641cce
junbyeol <junsung.yoon@devsisters.com>
2023-08-11 19:54:16 +0900

    Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
-import { CatsModule } from './cats/cats.module';
+import {MiddlewareConsumer, Module, NestModule, RequestMethod} from '@nestjs/common';
+import {CatsModule} from './cats/cats.module';
+import {LoggerMiddleware} from "./common/middlewares/logger.middleware";
+import {CatsController} from "./cats/cats.controller";
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
+  }
+}
