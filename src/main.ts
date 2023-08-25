bd21fe8baa375e4618541f436649c2d9775f6611
junbyeol <junsung.yoon@devsisters.com>
2023-08-25 16:54:23 +0900

    validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import {ValidationPipe} from "./common/pipes/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
